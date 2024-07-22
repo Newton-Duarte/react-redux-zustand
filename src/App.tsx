36d9eb72ba9da40b8b8f2e688fefaa0417df7cfa
Newton Duarte <newton_@hotmail.com>
2024-07-22 09:31:19 -0300

    feat: add redux provider and components to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Provider as ReduxProvider } from 'react-redux';
+import { store } from './store';
+import { AddTodo } from './components/add-todo';
+import { TodoList } from './components/todo-list';
+
 export function App() {
   return (
-    <>
-      <h1>Hello Redux</h1>
-    </>
+    <ReduxProvider store={store}>
+      <AddTodo />
+      <TodoList />
+    </ReduxProvider>
   )
 }
