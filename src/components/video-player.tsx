a1b5a29001035648387a3ae242d85f3e86722ed9
Newton Duarte <newton_@hotmail.com>
2024-07-22 15:16:24 -0300

    feat: add autoplay next lesson when video ends

--- src/components/video-player.tsx
+++ src/components/video-player.tsx
@@ -1,7 +1,11 @@
 import ReactPlayer from 'react-player'
 import { useAppSelector } from '../store';
+import { useDispatch } from 'react-redux';
+import { next } from '../store/slices/player';
 
 export function VideoPlayer() {
+  const dispatch = useDispatch()
+
   const lesson = useAppSelector((store) => {
     const currentModuleIndex = store.player.currentModuleIndex
     const currentLessonIndex = store.player.currentLessonIndex
@@ -15,6 +19,7 @@ export function VideoPlayer() {
         width="100%"
         height="100%"
         controls
+        onEnded={() => dispatch(next())}
         url={`https://www.youtube.com/watch?v=${lesson.id}`}
       />
     </div>
