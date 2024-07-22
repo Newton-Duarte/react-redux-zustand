676c1da8877c8c17b9f5eaf5a15a4b5251f44016
Newton Duarte <newton_@hotmail.com>
2024-07-22 15:05:35 -0300

    feat: add current video id to video player

--- src/components/video-player.tsx
+++ src/components/video-player.tsx
@@ -1,13 +1,21 @@
 import ReactPlayer from 'react-player'
+import { useAppSelector } from '../store';
 
 export function VideoPlayer() {
+  const lesson = useAppSelector((store) => {
+    const currentModuleIndex = store.player.currentModuleIndex
+    const currentLessonIndex = store.player.currentLessonIndex
+
+    return store.player.course.modules[currentModuleIndex].lessons[currentLessonIndex]
+  })
+
   return (
     <div className="w-full bg-zinc-950 aspect-video">
       <ReactPlayer
         width="100%"
         height="100%"
         controls
-        url="https://www.youtube.com/watch?v=Jai8w6K_GnY"
+        url={`https://www.youtube.com/watch?v=${lesson.id}`}
       />
     </div>
   );
