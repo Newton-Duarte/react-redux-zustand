da9a7267b4bb27cd2d59552b094ca1620b609d98
Newton Duarte <newton_@hotmail.com>
2024-07-22 10:59:04 -0300

    feat: add tailwindcss config

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
+import './styles/global.css';
 import { Provider as ReduxProvider } from 'react-redux';
 import { store } from './store';
-import { AddTodo } from './components/add-todo';
-import { TodoList } from './components/todo-list';
+// import { AddTodo } from './components/add-todo';
+// import { TodoList } from './components/todo-list';
+import { Player } from './components/player';
+
 
 export function App() {
   return (
     <ReduxProvider store={store}>
-      <AddTodo />
-      <TodoList />
+      {/* <AddTodo />
+      <TodoList /> */}
+      <Player />
     </ReduxProvider>
   )
 }
