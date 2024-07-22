9735c88e94b9629550c55f76e78896235bedbffa
Newton Duarte <newton_@hotmail.com>
2024-07-22 14:44:09 -0300

    feat: add player slice

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,6 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 import { TypedUseSelectorHook, useSelector } from "react-redux";
+import { player } from "./slices/player";
 
 const todoSlice = createSlice({
   name: 'todo',
@@ -13,7 +14,8 @@ const todoSlice = createSlice({
 
 export const store = configureStore({
   reducer: {
-    todo: todoSlice.reducer
+    todo: todoSlice.reducer,
+    player
   }
 })
 
