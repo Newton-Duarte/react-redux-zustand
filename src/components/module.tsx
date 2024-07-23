75f88bc3131d74942e4e3c6aaced66953968a71d
Newton Duarte <newton_@hotmail.com>
2024-07-23 09:27:22 -0300

    feat: use app dispatch and add null check for module component

--- src/components/module.tsx
+++ src/components/module.tsx
@@ -12,7 +12,7 @@ interface ModuleProps {
 }
 
 export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
-  const lessons = useAppSelector((store) => store.player.course.modules[moduleIndex].lessons)
+  const lessons = useAppSelector((store) => store.player.course?.modules[moduleIndex].lessons)
   const dispatch = useAppDispatch()
 
   const { currentModuleIndex, currentLessonIndex } = useAppSelector(state => {
@@ -38,7 +38,7 @@ export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
 
       <Collapsible.Content>
         <nav className="relative flex flex-col gap-4 p-6">
-          {lessons.map((lesson, lessonIndex) => {
+          {lessons && lessons.map((lesson, lessonIndex) => {
             const isCurrent = currentModuleIndex === moduleIndex &&
             currentLessonIndex === lessonIndex
 
